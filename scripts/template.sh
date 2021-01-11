caad0b2c5e53dcd004807b35a47dab65e34dc421
Andre Busche <andre.busche@gmx.de>
2021-01-11 20:23:05 +0100

    remove direct du calculation

--- scripts/template.sh
+++ scripts/template.sh
@@ -251,13 +251,13 @@ for SOURCE in "${SOURCES[@]}"
                 done
 
                 # perform backup size calculation
-                if [ ! $DRY_RUN = 0 ] && [ $ERROR = 0 ]; then
-                        backup_size=`eval $ducommand | cut -d" " -f1`
-                        echo -n "$0: Backup size of ${SOURCE} is "  >> $SUMMARYLOG 2>&1
-                        echo $backup_size | cut -d" " -f1 >> $SUMMARYLOG 2>&1
-                        echo -n "$0: Backup size of ${SOURCE} is "  >> $DETAILLOG 2>&1
-                        echo $backup_size | cut -d" " -f1 >> $DETAILLOG 2>&1
-                fi
+                #if [ ! $DRY_RUN = 0 ] && [ $ERROR = 0 ]; then
+                #        backup_size=`eval $ducommand | cut -d" " -f1`
+                #        echo -n "$0: Backup size of ${SOURCE} is "  >> $SUMMARYLOG 2>&1
+                #        echo $backup_size | cut -d" " -f1 >> $SUMMARYLOG 2>&1
+                #        echo -n "$0: Backup size of ${SOURCE} is "  >> $DETAILLOG 2>&1
+                #        echo $backup_size | cut -d" " -f1 >> $DETAILLOG 2>&1
+                #fi
                 echo "Sleeping for 5 seconds"
                 sleep 5
 done
